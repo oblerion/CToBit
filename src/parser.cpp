dbe0844ce731aae5682c335c51c5b04a2df90409
oblerion <82583559+oblerion@users.noreply.github.com>
2021-09-08 12:48:52 +0200

    Update parser.cpp
    some easing

--- src/parser.cpp
+++ src/parser.cpp
@@ -9,7 +9,6 @@ vector<string> Parser(string reading)
 
 	int tmp;
 	string stmp;
-	int i_ins=0,i_para=0;
 	
 	while(i<(signed int)reading.size())
 	{
@@ -19,43 +18,16 @@ vector<string> Parser(string reading)
 		   reading[i]=='\n') i+=1;
 		else
 		{
-			
 			if(string_find_list(reading,token,i)>-1)
 			{
 				j=string_find_list(reading,token,i);
-				if(token[j].size()==1)
-				{
-					vdata.push_back(token[j]);
-
-					// if(token[j][0]=='(')
-					// {
-					// stmp= "#p"+ to_string(i_para);
-					// 	vdata.push_back(stmp);
-					// 	printf("parentese ( en %d\n",i);
-					// 	stmp = get_all_btw(reading,"(",")",i);
-					// 	puts(stmp.c_str());
-					// 	vpara.push_back(stmp);
-					// 	i=i+stmp.size();
-					// 	i_para++;
-					// }
-					// else if(token[j][0]=='{')
-					// {
-					// 	stmp= "#i" + to_string(i_ins);
-					// 	vdata.push_back(stmp);
-					// 	stmp=get_all_btw(reading,"{","}",i);
-					// 	if(stmp[0]=='\n') stmp.erase(0,1);
-					// 	vinstru.push_back(stmp);
-					// 	i=i+stmp.size();
-					// 	i_ins++;
-					// }
-					// else 
-					i+= token[j].size();
-				}
-				else if(reading[i+token[j].size()]==' ')
+				//si token  
+				if(token[j].size()==1 || reading[i+token[j].size()]==' ')
 				{
 					vdata.push_back(token[j]);
 					i+= token[j].size();
 				}
+				// si false token
 				else
 				{
 					tmp=i+token[j].size();
@@ -76,7 +48,7 @@ vector<string> Parser(string reading)
 					i=tmp;
 				}
 			}
-			
+			// si not token
 			else 
 			{
 				tmp=i;
